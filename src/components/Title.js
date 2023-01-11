e8497b1f55f4c34c094752d868307a3494596d3c
clxrityy <97744702+clxrityy@users.noreply.github.com>
2023-01-11 14:52:07 -0500

    initial commit

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Title.css';
+
+const Title = () => {
+    return (
+        <div>
+            <div id="neons">
+                <h1>divinity</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Title;
---
+++ src/constants/index.js
@@ -0,0 +1,25 @@
+import { BsInstagram, BsTwitter, BsPinterest } from 'react-icons/bs';
+
+export const socailLinks = [
+    {
+        id: 'socialIcon',
+        icon: <BsInstagram 
+            style={{ hight: '50px', width:' 50px', color: '#f8f8f8'}}
+        />,
+        link: "https://www.instagram.com/caseymanglin/?hl=en"
+    },
+    {
+        id: 'socialIcon',
+        icon: <BsTwitter
+            style={{ hight: '50px', width:' 50px', color: '#f8f8f8'}}
+        />,
+        link: "/"
+    },
+    {
+        id: 'socialIcon',
+        icon: <BsPinterest
+            style={{ hight: '50px', width:' 50px', color: '#f8f8f8'}}
+        />,
+        link: "/"
+    }
+]
