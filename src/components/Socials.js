6cd7f1ec263c6f0b569994bcd36342167d0d71c0
clxrityy <97744702+clxrityy@users.noreply.github.com>
2023-01-13 15:55:16 -0500

    grid

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -3,7 +3,7 @@ import { socailLinks } from '../constants';
 
 const Socials = () => {
     return (
-        <div className='flex flex-row pr-8 lg:pt-8 md:mb-8'>
+        <div className='grid grid-cols-3 lg:pt-8 md:mb-8'>
             {socailLinks.map((social, index)=> (
                 <div className='p-8 drop-shadow-md'>
                     <a id='social-icon' href={social.link}>
--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -6,28 +6,13 @@ import { GiEarthAmerica } from 'react-icons/gi';
 
 const Footer = () => {
     return (
-        <div id='Footer' className='w-full'>
-            <div className='flex-wrap flex flex-col md:flex-row justify-between'>
+        <div id='Footer' className='w-full h-full'>
+            <div className='grid grid-cols-2 justify-between absolute bottom-0 w-full'>
                 <div className='mb-10 ml-5'>
                     <Title />
                 </div>
-                <div className='flex flex-row lg:flex-col align-middle justify-center text-center'>
-                    <div className='justify-center align-middle'>
-                        <Socials />
-                    </div>
-                    <div id='clxrity' className='flex flex-row justify-center align-middle right mt-8'>
-                        <GiEarthAmerica
-                            style={{ height: '20px', width: '20px', color: '#ffffff' }}
-                            className='mr-2 sm:mb-2 drop-shadow-md'
-                            id='earth-icon'
-                        />
-                        <h1
-                            className='font-semibold text:xs text-ash-white-light mb-5'
-                        >
-                            Built in partnership with <a href='https://clxrity.xyz'>clxrity</a>
-                        </h1>
-                        
-                    </div>
+                <div id='socials-div' className='right-0'>
+                    <Socials />
                 </div>
             </div>
         </div>
