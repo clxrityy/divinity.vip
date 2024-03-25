a316a2dc5e5005c783e6c8efec441ebe3901adfe
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-03-25 16:16:30 -0400

    testing more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,23 +33,32 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-touch-fullscreen" content="yes" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta property="og:title" content="divinity" />
-        <meta property="og:type" content="website" />
-        <meta property="og:description" content="a divine place to be anonymous" />
+        <meta name="og:title" content="divinity" />
+        <meta name="og:type" content="website" />
+        <meta name="og:description" content="a divine place to be anonymous" />
         <meta name="theme-color" content="#52789a" />
-        <meta property="og:image" content={`https://www.divinity.vip/favicon-32x32.png`} />
-        <meta property="og:image:alt" content="diamond logo icon" />
-        <meta property="og:image:width" content="32" />
-        <meta property="og:image:height" content="32" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="twitter:image" content={`https://www.divinity.vip/favicon-32x32.png`} />
-        <meta property="twitter:card" content="summary" />
-        <meta property="twitter:title" content="divinity" />
-        <meta property="twitter:creator" content="@yourclxrity" />
-        <meta property="twitter:description" content="a divine place to be anonymous" />
-        <meta property="description" content="share divine notes with the world anonymously" />
-        <meta property="og:url" content="https://divinity.vip" />
+        <meta name="og:image" content={`https://www.divinity.vip/favicon-32x32.png`} />
+        <meta name="og:image:alt" content="diamond logo icon" />
+        <meta name="og:image:width" content="32" />
+        <meta name="og:image:height" content="32" />
+        <meta name="og:image:type" content="image/png" />
+        <meta name="twitter:image" content={`https://www.divinity.vip/favicon-32x32.png`} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="divinity" />
+        <meta name="twitter:creator" content="@yourclxrity" />
+        <meta name="twitter:description" content="a divine place to be anonymous" />
+        <meta name="description" content="share divine notes with the world anonymously" />
+        <meta name="og:url" content="https://divinity.vip" />
+        <meta name="keywords" content="divinity, vip, anonymity, anonymous, confessions, advice, stories, global, secret, divine, theories, clxrity, georgia, athens, universe, explained" />
+        <meta name="og:country-name" content="USA" />
+        <meta name="og:region" content="GA" />
+        <meta name="og:postal-code" content="30606" />
+        <meta name="subject" content="divine anonymous  notes to the world" />
+        <meta name="target" content="all" />
+        <meta name="og:email" content="contact@mjanglin.com" />
+        <meta name="news_keywords" content="anonymous, divine, universe" />
       </head>
       <body className={inter.className}>
         <Navbar />
